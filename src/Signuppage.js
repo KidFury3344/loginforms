86942b25db1b32c5cf3221f798323be60bdcbf63
KidFury3344 <47640402+KidFury3344@users.noreply.github.com>
2021-04-16 12:05:56 +0300

    react-hook fixed

--- src/Signuppage.js
+++ src/Signuppage.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import "./Signuppage.css";
+import { useForm } from 'react-hook-form'
 function Signuppage() {
+    const {register,handleSubmit,errors }= useForm();
+    const onSubmit = (data)=>console.log(data);
     return (
-        <div className="outer">
-            <div className="inner">
-                <img className="logo" src="https://www.tpbproxypirate.com/images/pirat.jpg" alt="mainlogo"/>
-                <input className="inputs" name="username" placeholder="Username"/>
-                <input className="inputs" name="Email" placeholder="Email"/>
-                <input className="inputs" name="Password" placeholder="Password" type='password' autoComplete="on" />
-                <button className="btn-1" type="submit">Sign Up!</button>
+        <form onSubmit= {handleSubmit(onSubmit)}>
+            <div className="outer">
+                <div className="inner">
+                    <img className="logo" src="https://www.tpbproxypirate.com/images/pirat.jpg" alt="mainlogo"/>
+                    <input className="inputs" name="username" placeholder="Username" ref={register({required: true})}/>
+                    <input className="inputs" name="Email" placeholder="Email" ref={register({required: true})}/>
+                    <input className="inputs" name="Password" placeholder="Password" type='password' autoComplete="on" ref={register({required: true,minLength:8})} />
+                    <button className="btn-1" type="submit">Sign Up!</button>
+                </div>
             </div>
-        </div>
-    )
+        </form>
+    );
 }
 
 export default Signuppage
