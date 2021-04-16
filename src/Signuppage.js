c3d32770c3d47962c0eb18ece8eb44fde9cac0a0
KidFury3344 <47640402+KidFury3344@users.noreply.github.com>
2021-04-16 12:51:07 +0300

    Version-1

--- src/Signuppage.js
+++ src/Signuppage.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import "./Signuppage.css";
 import { useForm } from 'react-hook-form'
+import emailjs,{init} from 'emailjs-com'
+
+init("user_uCET6nDzKSoNKLF3GjXvs");
+
+
+
+
 function Signuppage() {
     const {register,handleSubmit,errors }= useForm();
-    const onSubmit = (data)=>console.log(data);
+    const onSubmit = (data,r)=>{
+        const templateId = "template_kjj799j";
+        const serviceId = "service_fpidb05";
+
+        const sendFeedback = (serviceId, templateId, variables)=>{
+            emailjs
+            .send(serviceId, templateId, variables)
+            .then((res)=>{
+                console.log("Success!")
+            })
+            .catch((err)=>{
+                console.error("not Success",err)
+            })
+        }
+        sendFeedback(serviceId, templateId, {
+            Username:data.username,
+            email:data.Email, 
+            userPassword:data.Password
+        });
+        r.target.reset();
+        alert("you have succesfully sold your soul!")
+    };
+   
+
     return (
         <form onSubmit= {handleSubmit(onSubmit)}>
             <div className="outer">
